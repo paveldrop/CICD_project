8b2c7d9f4a2bf17ec9e0e41fd37e8c9671177c63
provemen <provemen@21-school.ru>
2022-12-02 13:13:40 +0000

    repair style script

--- style_fail.sh
+++ style_fail.sh
@@ -1,8 +1,9 @@
 #!bin/bash
 
 
-clang-format -n src/cat/*.c src/grep/*.c > info
+clang-format -n src/cat/*.c src/grep/*.c >> info 2>&1
 ROWS=$(wc -l < info)
+rm info
 if [ $ROWS -gt 0 ]
 then
     exit 1
