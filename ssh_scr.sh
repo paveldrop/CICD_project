7c10c54ffb03ec77405243ef2eeafc637f4719fa
provemen <provemen@21-school.ru>
2022-12-04 19:25:02 +0000

    final

---
+++ ssh_scr.sh
@@ -0,0 +1,5 @@
+!#/bin/bash
+
+scp -p22 -o stricthostkeychecking=no -o userknownhostsfile=/dev/null ./src/cat/s21_cat provemen_deploy@10.20.0.20:/home/provemen_deploy
+scp -p22 -o stricthostkeychecking=no -o userknownhostsfile=/dev/null ./src/grep/s21_grep provemen_deploy@10.20.0.20:/home/provemen_deploy
+ssh -p22 provemen_deploy@10.20.0.20 "echo -e "kidalovo" | sudo -S mv s21* /usr/local/bin"
