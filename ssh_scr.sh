f7c24dc9786b61f399867963a0d36f0a6967a354
provemen <provemen@21-school.ru>
2022-12-04 19:26:33 +0000

    final

--- ssh_scr.sh
+++ ssh_scr.sh
@@ -1,4 +1,4 @@
-!#/bin/bash
+#!/bin/bash
 
 scp -p22 -o stricthostkeychecking=no -o userknownhostsfile=/dev/null ./src/cat/s21_cat provemen_deploy@10.20.0.20:/home/provemen_deploy
 scp -p22 -o stricthostkeychecking=no -o userknownhostsfile=/dev/null ./src/grep/s21_grep provemen_deploy@10.20.0.20:/home/provemen_deploy
