d3dbd28bd3f1f043bfe5ff8c7b452d75f55fe82f
provemen <provemen@21-school.ru>
2022-12-04 15:14:47 +0000

    part5.2

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
 
-scp src/cat/s21_cat provemen_deploy@10.20.0.20:/home/provemen_deploy
-scp src/grep/s21_grep provemen_deploy@10.20.0.20:/home/provemen_deploy
-ssh provemen_deploy@10.20.0.20
-cd /home/provemen_deploy
-mv s21* /usr/local/bin
+scp -p22 src/cat/s21_cat provemen_deploy@10.20.0.20:/home/provemen_deploy
+scp -p22 src/grep/s21_grep provemen_deploy@10.20.0.20:/home/provemen_deploy
+ssh -p22 provemen_deploy@10.20.0.20 "ls -a"
