4012bd23535f7f93777ed0c2e671e373b1d00878
provemen <provemen@21-school.ru>
2022-12-05 14:20:38 +0000

    fix tg message and git ci

--- tg_msg.sh
+++ tg_msg.sh
@@ -4,6 +4,6 @@ TOKEN=5936812653:AAHweJfKK_a7Xr7X73TuvBu16yjWAqJFY24
 ID=160290591
 TIME="10"
 URL="https://api.telegram.org/bot$TOKEN/sendMessage"
-TEXT="Stage status: $1%0AStage:+$CI_JOB_STAGE%0AProject:+$CI_PROJECT_NAME%0AURL:+$CI_PROJECT_URL/pipelines/$CI_PIPELINE_ID/%0ABranch:+$CI_COMMIT_REF_SLUG"
+TEXT="Status CI/CD: *$1* %0AStage:+$CI_JOB_STAGE%0AProject:+$CI_PROJECT_NAME%0AURL:+$CI_PROJECT_URL/pipelines/$CI_PIPELINE_ID/%0ABranch:+$CI_COMMIT_REF_SLUG"
 
 curl -s --max-time $TIME -d "chat_id=$ID&disable_web_page_preview=1&text=$TEXT" $URL > /dev/null
